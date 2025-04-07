b21f254e81a9fb29fa49b34c216830ab17fea6bb
NicolasVegaQ <venico222211@gmail.com>
2025-04-06 22:12:19 -0500

    integrar base datos y la auth

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import os
+
 from flask import Flask
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -8,3 +10,29 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
